3f8a5e0428f5946d35f687ed5f4328673d5fe3de
Diego <jdiegogarciaco@gmail.com>
2022-10-06 11:59:38 +0200

    Add index.html style.css script.js images/ design/

---
+++ script.js
@@ -0,0 +1,18 @@
+const plus = document.querySelector(".plus"),
+    minus = document.querySelector(".minus"),
+    num = document.querySelector(".num");
+
+let a = 0;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    num.innerText = a;
+    console.log(a);
+});
+
+minus.addEventListener("click", ()=>{
+    if(a > 0){
+        a--;
+        num.innerText = a;
+    }
+});
